88635fe87eed8ecfeb5222977fdf1985fa6cc7c8
Maxwell Benton <maxwellbenton@gmail.com>
2017-12-10 14:43:18 -0500

    basic hexagonal tiling, using random data

--- src/Map.js
+++ src/Map.js
@@ -3,27 +3,68 @@ import GoogleMapReact from "google-map-react";
 
 class Map extends Component {
   state = {
-    positionArray: [],
-    colors: ["yellow", "black", "red", "blue"]
+    positionArray: []
   };
 
   onMapChange(center, zoom, bounds, marginBounds) {
-    let lat = Math.round(center[0] * 1000) / 1000;
-    let long = Math.round(center[1] * 1000) / 1000;
-    let latBoundCount = Math.abs((bounds[0] - bounds[2]) * 1000);
-    let longBoundCount = Math.abs((bounds[1] - bounds[3]) * 1000);
-    let wWidth = Math.round(1 / longBoundCount * 100);
-    let wHeight = Math.round(1 / latBoundCount * 100);
-    console.log(wWidth * longBoundCount);
-    console.log(wHeight * latBoundCount);
+    //store lat/long for upper left corner of map
+    let cornerLat = Math.round(bounds[0] * 1000) / 1000;
+    let cornerLong = Math.round(bounds[1] * 1000) / 1000;
+
+    //calculate the lat/long difference between the upper left and lower right
+    //if every tile is centered on a coordinate from the db, this
+    //* 1000 = 0.001 degrees between lat/long points, * 2000 = 0.0005 degrees (smaller tiles)
+    let latBoundCount = Math.abs((bounds[0] - bounds[2]) * 2000);
+    let longBoundCount = Math.abs((bounds[1] - bounds[3]) * 2000);
+
+    let wWidth = Math.round(1 / longBoundCount * 100) + 0.3;
+    let wHeight = Math.round(1 / latBoundCount * 100) + 0.35;
+
     let pArray = [];
-    for (let i = -latBoundCount; i < latBoundCount; i += 1) {
-      for (let n = -longBoundCount; n < longBoundCount; n += 1) {
+    for (let i = 0; i < latBoundCount + 1; i += 1) {
+      for (let n = 0; n < longBoundCount + 1; n += 1) {
+        let r = Math.floor(Math.random() * 16);
+        let color;
+        let opacity = "0.9";
+        switch (r) {
+          case 0:
+          case 1:
+          case 2:
+          case 3:
+          case 4:
+          case 5:
+          case 6:
+          case 7:
+            color = "black";
+            break;
+          case 8:
+          case 9:
+          case 10:
+            color = "yellow";
+            break;
+          case 11:
+            color = "blue";
+            break;
+          case 12:
+            color = `#${Math.floor(Math.random() * 10)}${Math.floor(
+              Math.random() * 10
+            )}${Math.floor(Math.random() * 10)}`;
+            break;
+          case 13:
+          case 14:
+          case 15:
+          default:
+            opacity = "0";
+        }
+
         pArray.push({
-          latitude: lat + i / 1000,
-          longitude: long + n / 1000,
-          width: wWidth + "vw",
-          height: wHeight + "vh"
+          latitude:
+            n % 2 === 0 ? cornerLat - i / 2000 : cornerLat - i / 2000 + 0.00025,
+          longitude: cornerLong + n / 2000,
+          width: wWidth,
+          color: color,
+          height: wHeight,
+          opacity: 0.8
         });
       }
     }
@@ -40,15 +81,18 @@ class Map extends Component {
           lat={position.latitude}
           lng={position.longitude}
           style={{
-            width: position.width,
-            height: position.height,
-            backgroundColor: this.state.colors[
-              Math.floor(Math.random() * this.state.colors.length)
-            ],
-            opacity: "0.5",
+            width: position.width + position.width / 3 + "vw",
+            height: position.height + "vh",
+            clipPath:
+              "polygon(75% 0, 100% 50%, 75% 100%, 25% 100%, 0 50%, 25% 0)",
+            backgroundColor: position.color,
+
+            opacity: position.opacity,
             transform: "translateX(-50%) translateY(-50%)"
           }}
-        />
+        >
+          [{position.latitude}, {position.longitude}]
+        </div>
       );
     });
     console.log(gridPoints);
@@ -62,7 +106,7 @@ class Map extends Component {
           options={{ mapTypeId: "satellite" }}
           onBoundsChange={this.onMapChange.bind(this)}
           center={[this.props.latitude, this.props.longitude]}
-          defaultZoom={16}
+          defaultZoom={18}
         >
           {gridPoints}
         </GoogleMapReact>
