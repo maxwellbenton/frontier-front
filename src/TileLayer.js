d7a10dea80ca6b44a8307ed68b0c2b6d91353245
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-12-13 15:10:00 -0500

    almost there just doing snap to now

--- src/TileLayer.js
+++ src/TileLayer.js
@@ -7,8 +7,8 @@ class TileLayer extends Component {
     divWidth: 100,
     spacing: 75,
     divHeight: 100,
-    xTiles: 25,
-    yTiles: 15
+    xTiles: 10,
+    yTiles: 8
   };
 
   componentDidMount() {
@@ -34,32 +34,32 @@ class TileLayer extends Component {
   }
 
   render() {
-    const tileDivs = this.state.tileArray.map((tile, index) => {
-      return (
-        <div
-          key={index}
-					rowidx={tile.rowIdx}
-					colidx={tile.colIdx}
+    const tileDivs = this.state.tileArray.map((tile, index) => (
+      <div
+        key={index}
+				rowidx={tile.rowIdx}
+				colidx={tile.colIdx}
 
-          className="tile"
-          style={{
-            top:
-              tile.y,
-            left:
-              tile.x,
-            width: this.state.divWidth,
-            height: this.state.divHeight,
-            clipPath: "polygon(75% 0%, 99% 50%, 75% 99%, 25% 99%, 1% 50%, 25% 0%)",
-            backgroundColor: tile.color,
-            lineHeight: this.state.divHeight,
-            textAlign: "center",
-            opacity: 0.9,
-            transform: "translateX(-50%) translateY(-50%)"
-          }}
-        />
-      );
-    });
-    return <div id="tile-map" className="dragscroll">{tileDivs}</div>;
+        className="tile"
+        style={{
+          top:
+            tile.y,
+          left:
+            tile.x,
+          width: this.state.divWidth,
+          height: this.state.divHeight,
+          clipPath: "polygon(75% 0%, 99% 50%, 75% 99%, 25% 99%, 1% 50%, 25% 0%)",
+          backgroundColor: tile.color,
+          lineHeight: this.state.divHeight,
+          textAlign: "center",
+          opacity: 0.9,
+        }}
+      />
+    ))
+		const width = (this.state.divWidth * 0.75) * this.state.xTiles + this.state.divWidth/4
+		const height = this.state.divHeight * this.state.yTiles + this.state.divHeight/2
+    return <div id="tile-map" style={{width: `${width}px`, height: `${height}px`}}
+		className="dragscroll">{tileDivs}</div>;
   }
 
   handleMouseDown = e => {
