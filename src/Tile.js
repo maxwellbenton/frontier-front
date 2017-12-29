2ad7e5f0095977dc709ccec752e24490f5c32f15
Maxwell Benton <maxwellbenton@gmail.com>
2017-12-29 13:16:23 -0500

    aligned with imperfections lat long

--- src/Tile.js
+++ src/Tile.js
@@ -1,28 +1,36 @@
-import React from "react"
-import TileBorder from "./TileBorder.js"
+import React from "react";
+import TileBorder from "./TileBorder.js";
 
-const Tile = ({rowIdx, colIdx, tileHeight, tileWidth, offset, latitude, longitude, data}) => {
-
+const Tile = ({
+  rowIdx,
+  colIdx,
+  tileHeight,
+  tileWidth,
+  xPos,
+  yPos,
+  latitude,
+  longitude,
+  data
+}) => {
   return (
     <div
       rowidx={rowIdx}
       colidx={colIdx}
       className="tile"
       style={{
-        top: rowIdx * tileHeight + offset,
-        left: colIdx * tileWidth,
+        position: "absolute",
+        top: yPos,
+        left: xPos,
         width: tileWidth,
         height: tileHeight,
-        opacity: 0.7
+        opacity: 0.3
       }}
     >
       <div>{data}</div>
       <div>{latitude}</div>
       <div>{longitude}</div>
-      <TileBorder width={tileWidth*1.2} height={tileHeight*1.2} color={(Math.round(Math.random())) ? '#faa' : '#373'}/>
     </div>
-  )
+  );
+};
 
-}
-
-export default Tile
+export default Tile;
